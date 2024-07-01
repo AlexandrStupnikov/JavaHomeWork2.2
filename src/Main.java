ecb254cd5a56dd6a3327ee7facf88210e0cade09
AleksandrStupnikov <a-a-stupnikov@yandex-team.ru>
2024-07-01 15:09:18 +0300

    hw2.2.2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int initial_Balance = 100; // начальный баланс
-        int replenishment_Of_The_Balance = 1100; // пополнение баланса
-        int total_Balance = initial_Balance + replenishment_Of_The_Balance; //общий баланс
+        int initialBalance = 100; // начальный баланс
+        int replenishmentOfTheBalance = 1100; // пополнение баланса
+        int totalBalance = initialBalance + replenishmentOfTheBalance; //общий баланс
         int bonus;
-        if (total_Balance > 1000) {
-            bonus = ((total_Balance - initial_Balance) / 100);
+        if (totalBalance > 1000) {
+            bonus = ((totalBalance - initialBalance) / 100);
         } else {
             bonus = 0;
         }
         System.out.println("Начисленно бонусов " + bonus);
-        System.out.println("Ваш баланс " + (bonus + total_Balance) + " рублей");
+        System.out.println("Ваш баланс " + (bonus + totalBalance) + " рублей");
     }
 }
